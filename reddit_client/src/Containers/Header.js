5c840ecefc85353927485244ce0963ab4a6f079e
Mateusz Polis <polismateusz@gmail.com>
2022-12-28 00:06:52 +0100

    Add Header, Main and Footer containers

---
+++ reddit_client/src/Containers/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Header.css';
+
+export const Header = () => {
+    return (
+        <header>
+            <h1>This is the header</h1>
+        </header>
+    );
+}
---
+++ reddit_client/src/Containers/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Main.css'; 
+
+export const Main = () => {
+    return (
+        <main>
+            <h1>This is the main</h1>
+        </main>
+    )
+}
