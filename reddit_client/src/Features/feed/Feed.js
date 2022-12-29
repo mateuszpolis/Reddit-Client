e1f9b0f1ae47de5be2e6e12a1e517f73bc6f5356
Mateusz Polis <polismateusz@gmail.com>
2022-12-29 20:35:33 +0100

    Add custom scrollbar

--- reddit_client/src/Features/feed/Feed.js
+++ reddit_client/src/Features/feed/Feed.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Post } from "../../Components/Post";
 import "./Feed.css";
+import "./ScrollBar.css";
 import imageTest from "../../images/photo-1481349518771-20055b2a7b24.jpg";
 
 export const Feed = () => {
@@ -20,6 +21,18 @@ export const Feed = () => {
         text="Donec pellentesque odio nec lacus convallis rutrum. Integer et leo id augue interdum volutpat non eu nulla. Donec fermentum porta ornare. Vestibulum odio dui, fermentum vel maximus in, rutrum vitae dui. Phasellus fringilla et nisi vel gravida. Integer vel iaculis enim. Nulla facilisi.
 
 "
+      />
+      <Post
+        header="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris."
+        text="Sed ac ante iaculis orci dignissim tempus vel sed justo. Morbi non quam odio. Duis rhoncus varius vulputate. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed auctor, nisi ut interdum fringilla, orci diam convallis ipsum, vel pharetra eros nisi id justo. Quisque lacinia quis odio quis sagittis. Donec maximus sapien ac semper cursus. Etiam porta in nisl nec hendrerit. Curabitur eleifend magna mollis egestas consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus non sollicitudin dolor."
+      />
+      <Post
+        header="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris."
+        text="Sed ac ante iaculis orci dignissim tempus vel sed justo. Morbi non quam odio. Duis rhoncus varius vulputate. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed auctor, nisi ut interdum fringilla, orci diam convallis ipsum, vel pharetra eros nisi id justo. Quisque lacinia quis odio quis sagittis. Donec maximus sapien ac semper cursus. Etiam porta in nisl nec hendrerit. Curabitur eleifend magna mollis egestas consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus non sollicitudin dolor."
+      />
+      <Post
+        header="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris."
+        text="Sed ac ante iaculis orci dignissim tempus vel sed justo. Morbi non quam odio. Duis rhoncus varius vulputate. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed auctor, nisi ut interdum fringilla, orci diam convallis ipsum, vel pharetra eros nisi id justo. Quisque lacinia quis odio quis sagittis. Donec maximus sapien ac semper cursus. Etiam porta in nisl nec hendrerit. Curabitur eleifend magna mollis egestas consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus non sollicitudin dolor."
       />
     </div>
   );
