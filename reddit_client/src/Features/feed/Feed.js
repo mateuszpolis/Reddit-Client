4bb4e63336be0ed94c8d7c6a9b08e51311715a4e
Mateusz Polis <polismateusz@gmail.com>
2023-01-07 00:56:35 +0100

    Add scroll snapping to the feed

--- reddit_client/src/Features/feed/Feed.js
+++ reddit_client/src/Features/feed/Feed.js
@@ -14,5 +14,44 @@ export const Feed = () => {
   if (isLoading) {
     return <div id="feed" className="isLoading"></div>;
   }
-  return <div id="feed"></div>;
+  return (
+    <div id="feed">
+      <Post
+        header="Lorem ipsum dolor sit amet, consectetur."
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque, nisi ullamcorper fermentum mattis, quam metus molestie nisi, eu dapibus ante nisl in sem. Proin iaculis nisl sit amet tempus tincidunt. Nullam sit amet tincidunt nisl. Maecenas blandit ante id nisl maximus vehicula. Nulla laoreet finibus tincidunt. Pellentesque et enim urna. Phasellus id magna id elit porttitor suscipit. Fusce ipsum velit, facilisis vel pellentesque a, euismod in sem. Nullam feugiat, dolor ut vulputate convallis, arcu elit tincidunt nunc, ac pharetra purus lectus in libero. Ut vel mollis sapien, a lobortis nibh. Cras eget arcu nisi. Pellentesque dapibus nisl a fermentum rhoncus. Phasellus arcu purus, sodales ullamcorper lobortis at, faucibus in libero. Nullam vehicula sed magna quis pulvinar. Curabitur dui mauris, posuere eget arcu vel, vulputate vulputate erat. Sed bibendum a enim eu finibus.
+
+        "
+      />
+      <Post
+        header="Lorem ipsum dolor sit amet, consectetur."
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque, nisi ullamcorper fermentum mattis, quam metus molestie nisi, eu dapibus ante nisl in sem. Proin iaculis nisl sit amet tempus tincidunt. Nullam sit amet tincidunt nisl. Maecenas blandit ante id nisl maximus vehicula. Nulla laoreet finibus tincidunt. Pellentesque et enim urna. Phasellus id magna id elit porttitor suscipit. Fusce ipsum velit, facilisis vel pellentesque a, euismod in sem. Nullam feugiat, dolor ut vulputate convallis, arcu elit tincidunt nunc, ac pharetra purus lectus in libero. Ut vel mollis sapien, a lobortis nibh. Cras eget arcu nisi. Pellentesque dapibus nisl a fermentum rhoncus. Phasellus arcu purus, sodales ullamcorper lobortis at, faucibus in libero. Nullam vehicula sed magna quis pulvinar. Curabitur dui mauris, posuere eget arcu vel, vulputate vulputate erat. Sed bibendum a enim eu finibus.
+
+        "
+      />
+      <Post
+        header="Lorem ipsum dolor sit amet, consectetur."
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque, nisi ullamcorper fermentum mattis, quam metus molestie nisi, eu dapibus ante nisl in sem. Proin iaculis nisl sit amet tempus tincidunt. Nullam sit amet tincidunt nisl. Maecenas blandit ante id nisl maximus vehicula. Nulla laoreet finibus tincidunt. Pellentesque et enim urna. Phasellus id magna id elit porttitor suscipit. Fusce ipsum velit, facilisis vel pellentesque a, euismod in sem. Nullam feugiat, dolor ut vulputate convallis, arcu elit tincidunt nunc, ac pharetra purus lectus in libero. Ut vel mollis sapien, a lobortis nibh. Cras eget arcu nisi. Pellentesque dapibus nisl a fermentum rhoncus. Phasellus arcu purus, sodales ullamcorper lobortis at, faucibus in libero. Nullam vehicula sed magna quis pulvinar. Curabitur dui mauris, posuere eget arcu vel, vulputate vulputate erat. Sed bibendum a enim eu finibus.
+
+        "
+      />
+      <Post
+        header="Lorem ipsum dolor sit amet, consectetur."
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque, nisi ullamcorper fermentum mattis, quam metus molestie nisi, eu dapibus ante nisl in sem. Proin iaculis nisl sit amet tempus tincidunt. Nullam sit amet tincidunt nisl. Maecenas blandit ante id nisl maximus vehicula. Nulla laoreet finibus tincidunt. Pellentesque et enim urna. Phasellus id magna id elit porttitor suscipit. Fusce ipsum velit, facilisis vel pellentesque a, euismod in sem. Nullam feugiat, dolor ut vulputate convallis, arcu elit tincidunt nunc, ac pharetra purus lectus in libero. Ut vel mollis sapien, a lobortis nibh. Cras eget arcu nisi. Pellentesque dapibus nisl a fermentum rhoncus. Phasellus arcu purus, sodales ullamcorper lobortis at, faucibus in libero. Nullam vehicula sed magna quis pulvinar. Curabitur dui mauris, posuere eget arcu vel, vulputate vulputate erat. Sed bibendum a enim eu finibus.
+
+        "
+      />
+      <Post
+        header="Lorem ipsum dolor sit amet, consectetur."
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque, nisi ullamcorper fermentum mattis, quam metus molestie nisi, eu dapibus ante nisl in sem. Proin iaculis nisl sit amet tempus tincidunt. Nullam sit amet tincidunt nisl. Maecenas blandit ante id nisl maximus vehicula. Nulla laoreet finibus tincidunt. Pellentesque et enim urna. Phasellus id magna id elit porttitor suscipit. Fusce ipsum velit, facilisis vel pellentesque a, euismod in sem. Nullam feugiat, dolor ut vulputate convallis, arcu elit tincidunt nunc, ac pharetra purus lectus in libero. Ut vel mollis sapien, a lobortis nibh. Cras eget arcu nisi. Pellentesque dapibus nisl a fermentum rhoncus. Phasellus arcu purus, sodales ullamcorper lobortis at, faucibus in libero. Nullam vehicula sed magna quis pulvinar. Curabitur dui mauris, posuere eget arcu vel, vulputate vulputate erat. Sed bibendum a enim eu finibus.
+
+        "
+      />
+      <Post
+        header="Lorem ipsum dolor sit amet, consectetur."
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque, nisi ullamcorper fermentum mattis, quam metus molestie nisi, eu dapibus ante nisl in sem. Proin iaculis nisl sit amet tempus tincidunt. Nullam sit amet tincidunt nisl. Maecenas blandit ante id nisl maximus vehicula. Nulla laoreet finibus tincidunt. Pellentesque et enim urna. Phasellus id magna id elit porttitor suscipit. Fusce ipsum velit, facilisis vel pellentesque a, euismod in sem. Nullam feugiat, dolor ut vulputate convallis, arcu elit tincidunt nunc, ac pharetra purus lectus in libero. Ut vel mollis sapien, a lobortis nibh. Cras eget arcu nisi. Pellentesque dapibus nisl a fermentum rhoncus. Phasellus arcu purus, sodales ullamcorper lobortis at, faucibus in libero. Nullam vehicula sed magna quis pulvinar. Curabitur dui mauris, posuere eget arcu vel, vulputate vulputate erat. Sed bibendum a enim eu finibus.
+
+        "
+      />
+    </div>
+  );
 };
