b9cb5a70cab86f101475ee8282372c6e4b84187d
Mateusz Polis <polismateusz@gmail.com>
2023-01-09 19:34:52 +0100

    Add postInfo animation on hover

--- reddit_client/src/Features/sortBy/SortBy.js
+++ reddit_client/src/Features/sortBy/SortBy.js
@@ -2,13 +2,33 @@ import React from "react";
 import "./SortBy.css";
 
 export const SortBy = () => {
+  const handleMouseEnter = () => {
+    const postInfo = document.getElementById("postInfo");
+    postInfo.style.top = "30%";
+  };
+
+  const handleMouseLeave = () => {
+    const postInfo = document.getElementById("postInfo");
+    postInfo.style.top = "15%";
+  };
+
   return (
     <div id="sortWrapper">
-      <div id="sort" className="dropdownButton">
+      <div
+        id="sort"
+        className="dropdownButton"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
         <h4>Sort</h4>
         <i className="fa-solid fa-sort-down"></i>
       </div>
-      <div id="relevance" className="dropdownButton">
+      <div
+        id="relevance"
+        className="dropdownButton"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
         <h4>All time</h4>
         <i className="fa-solid fa-sort-down"></i>
       </div>
