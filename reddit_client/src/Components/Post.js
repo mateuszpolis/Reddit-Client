9435e8f112aeffa8dd7fbc371052bf12c830f8ed
Mateusz Polis <polismateusz@gmail.com>
2023-01-10 13:32:55 +0100

    Add iframe display

--- reddit_client/src/Components/Post.js
+++ reddit_client/src/Components/Post.js
@@ -25,10 +25,9 @@ export const Post = ({ post }) => {
   const handleToggleOverflow = () => {
     const pWrapper = document.getElementById(post.id + "d");
     const p = document.getElementById(post.id + "p");
-    console.log(pWrapper.style.height);
     if (
-      pWrapper.style.height === "fit-content" ||
-      pWrapper.style.height === ""
+      pWrapper.style.overflowY === "hidden" ||
+      pWrapper.style.overflowY === ""
     ) {
       pWrapper.style.overflowY = "scroll";
       p.innerHTML = post.selftext;
@@ -47,24 +46,56 @@ export const Post = ({ post }) => {
         </div>
       </div>
     );
-  } else if (post?.post_hint === "image") {
-    if (post.selftext.length !== 0) {
-      return (
-        <div className="post isImage" id={post.id}>
-          <h2>{post.title}</h2>
-          <div
-            className="isImageText"
-            id={post.id + "d"}
-            onClick={handleToggleOverflow}
-          >
-            <p id={post.id + "p"}>{truncateText(post.selftext, 100)} </p>
-          </div>
-          <div className="imageWrapper" id={post.id + "img"}>
-            <img src={post.url} alt={post.id} />
-          </div>
+  } else if (post?.post_hint === "image" && post.selftext.length !== 0) {
+    return (
+      <div className="post isImage" id={post.id}>
+        <h2>{post.title}</h2>
+        <div
+          className="isImageText"
+          id={post.id + "d"}
+          onClick={handleToggleOverflow}
+        >
+          <p id={post.id + "p"}>{truncateText(post.selftext, 100)} </p>
         </div>
-      );
-    }
+        <div className="imageWrapper" id={post.id + "img"}>
+          <img src={post.url} alt={post.id} />
+        </div>
+      </div>
+    );
+  } else if (post?.post_hint === "image") {
+    return (
+      <div className="post isImageFull" id={post.id}>
+        <h2>{post.title}</h2>
+        <div className="imageFullWrapper" id={post.id + "img"}>
+          <img src={post.url} alt={post.id} />
+        </div>
+      </div>
+    );
+  } else if (post?.post_hint === "rich:video") {
+    return (
+      <div className="post isVideo" id={post.id}>
+        <h2>{post.title}</h2>
+        <div className="videoWrapper" id={post.id + "vid"}>
+          <video autoPlay controls>
+            <source src={post.url} type="video/mp4"></source>
+          </video>
+        </div>
+      </div>
+    );
+  } else if (post?.post_hint === "hosted:video") {
+    return (
+      <div className="post isIframe" id={post.id}>
+        <h2>{post.title}</h2>
+        <div className="iframeWrapper" id={post.id + "vid"}>
+          <iframe
+            src={post.media.reddit_video.fallback_url}
+            title={post.title}
+            autoPlay
+            controls
+          />
+        </div>
+      </div>
+    );
   }
 
   return (
