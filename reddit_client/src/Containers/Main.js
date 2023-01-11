dd5ff56bd67cbbfa8b42182958773958a7673b4c
Mateusz Polis <polismateusz@gmail.com>
2023-01-11 18:22:22 +0100

    Add popular feature and mockup posts

--- reddit_client/src/Containers/Main.js
+++ reddit_client/src/Containers/Main.js
@@ -2,11 +2,13 @@ import React from "react";
 import { CommentSection } from "../Features/commentSection/CommentSection";
 import { Feed } from "../Features/feed/Feed";
 import { PostInformation } from "../Features/postInformation/PostInformation";
+import { Popular } from "../Features/popular/Popular";
 import "./Main.css";
 
 export const Main = () => {
   return (
     <main>
+      <Popular />
       <Feed />
       <PostInformation />
       <CommentSection />
---
+++ reddit_client/src/Features/popular/Popular.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Popular.css";
+
+export const Popular = () => {
+  return (
+    <div id="popularPosts">
+      <div className="popularPost"></div>
+      <div className="popularPost"></div>
+      <div className="popularPost"></div>
+      <div className="popularPost"></div>
+      <div className="popularPost"></div>
+      <div className="popularPost"></div>
+      <div className="popularPost"></div>
+      <div className="popularPost"></div>
+      <div className="popularPost"></div>
+    </div>
+  );
+};
