f5129225d4ea5d489c1d4e86981fd6fa8d295d8e
Mateusz Polis <polismateusz@gmail.com>
2023-01-11 18:51:07 +0100

    Add button to scroll right

--- reddit_client/src/Features/popular/Popular.js
+++ reddit_client/src/Features/popular/Popular.js
@@ -2,17 +2,34 @@ import React from "react";
 import "./Popular.css";
 
 export const Popular = () => {
+  const handleScrollRight = () => {
+    const wrapper = document.getElementById("popularPosts");
+    wrapper.scroll({
+      top: 0,
+      left: wrapper.scrollLeft + (wrapper.offsetWidth / 4),
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div id="popularPosts">
-      <div className="popularPost"></div>
-      <div className="popularPost"></div>
-      <div className="popularPost"></div>
-      <div className="popularPost"></div>
-      <div className="popularPost"></div>
-      <div className="popularPost"></div>
-      <div className="popularPost"></div>
-      <div className="popularPost"></div>
-      <div className="popularPost"></div>
+    <div id="popularPostsContainer">
+      <div id="popularPosts">
+        <div className="popularPost"></div>
+        <div className="popularPost"></div>
+        <div className="popularPost"></div>
+        <div className="popularPost"></div>
+        <div className="popularPost"></div>
+        <div className="popularPost"></div>
+        <div className="popularPost"></div>
+        <div className="popularPost"></div>
+        <div className="popularPost"></div>
+        <div className="popularPost"></div>
+      </div>
+      <div id="clickToScrollRight" onClick={handleScrollRight}>
+        <h5>
+          <i class="fa-solid fa-caret-right"></i>
+        </h5>
+      </div>
     </div>
   );
 };
