3458674b0bf74bcb35a1a6e3cad559c6e53e1acc
Mateusz Polis <polismateusz@gmail.com>
2023-01-13 00:01:33 +0100

    More precision when showing numbers

--- reddit_client/src/helperFunctions/functions.js
+++ reddit_client/src/helperFunctions/functions.js
@@ -12,7 +12,7 @@ export const intToString = (value) => {
   let suffixes = ["", "k", "m", "b", "t"];
   let suffixNum = Math.floor((("" + value).length - 1) / 3);
   let shortValue = parseFloat(
-    (suffixNum !== 0 ? value / Math.pow(1000, suffixNum) : value).toPrecision(2)
+    (suffixNum !== 0 ? value / Math.pow(1000, suffixNum) : value).toPrecision(3)
   );
   if (shortValue % 1 !== 0) {
     shortValue = shortValue.toFixed(1);
