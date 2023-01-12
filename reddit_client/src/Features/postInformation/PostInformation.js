e7e853cf5837adfc2174ed2f081e2801f6eebb07
Mateusz Polis <polismateusz@gmail.com>
2023-01-12 21:48:19 +0100

    Remake of the PostInformation section

--- reddit_client/src/Features/postInformation/PostInformation.js
+++ reddit_client/src/Features/postInformation/PostInformation.js
@@ -1,10 +1,7 @@
 import React, { useEffect } from "react";
 import "./PostInformation.css";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  findCurrentPost,
-  selectPostInformation,
-} from "./postinformationSlice";
+import { findCurrentPost, selectPostInformation } from "./postinformationSlice";
 import { hasLoadedPosts, isLoadingPosts } from "../feed/feedSlice";
 import picture from "../../images/photo-1481349518771-20055b2a7b24.jpg";
 import { intToString } from "../../helperFunctions/functions";
@@ -21,54 +18,38 @@ export const PostInformation = () => {
   useEffect(() => {}, [dispatch]);
 
   if (isLoading) {
-    return (
-      <div id="postInfo">
-        <div className="informationPart">
-          <div id="subredditImgPlaceholder" className="isLoading"></div>
-          <div id="subredditNamePlaceholder" className="isLoading"></div>
-          <div id="userNamePlaceholder" className="isLoading"></div>
-          <div id="datePlaceholder" className="isLoading"></div>
-        </div>
-        <div className="informationPart">
-          <i id="upvotes" className="fa-solid fa-heart"></i>
-          <div id="numberOfUpvotesPlaceholder" className="isLoading"></div>
-          <i id="downvotes" className="fa-solid fa-heart-crack"></i>
-        </div>
-      </div>
-    );
+    return <div id="postInfo" className="isLoading"></div>;
   } else if (hasLoaded) {
     return (
       <div id="postInfo">
-        <div className="informationPart">
-          <div id="subredditImgPlaceholder">
-            <img alt="subreddit logo" src={picture}/>
-          </div>
-          <div id="subredditNamePlaceholder" className="centerText">{data.subredditName}</div>
-          <div id="userNamePlaceholder" className="centerText">u/{data.userName}</div>
-          <div id="datePlaceholder" className="centerText">Added: {data.date}</div>
+        <div id="subredditNamePlaceholder" style={{ display: "inline-block" }}>
+          <h4>{data.subredditName}</h4>
         </div>
-        <div className="informationPart">
-          <i id="upvotes" className="fa-solid fa-heart"></i>
-          <div id="numberOfUpvotesPlaceholder">{intToString(data.votes)}</div>
-          <i id="downvotes" className="fa-solid fa-heart-crack"></i>
+        <div
+          id="userNamePlaceholder"
+          style={{
+            display: "inline-block",
+            position: "absolute",
+            right: "1vw",
+          }}
+        >
+          <h4>u/{data.userName}</h4>
+        </div>
+        <div id="numberOfUpvotesPlaceholder" style={{ margin: "2.7vh 0" }}>
+          <h4>
+            Upvotes: <i id="upvotes" className="fa-solid fa-heart"></i>{" "}
+            {intToString(data.votes)}{" "}
+            <i id="downvotes" className="fa-solid fa-heart-crack"></i>
+          </h4>
+        </div>
+        <div id="datePlaceholder">
+          <h5>
+            Added: <u>{data.date}</u>
+          </h5>
         </div>
       </div>
     );
   } else {
-    return (
-      <div id="postInfo">
-        <div className="informationPart">
-          <div id="subredditImgPlaceholder"></div>
-          <div id="subredditNamePlaceholder"></div>
-          <div id="userNamePlaceholder"></div>
-          <div id="datePlaceholder"></div>
-        </div>
-        <div className="informationPart">
-          <i id="upvotes" className="fa-solid fa-heart"></i>
-          <div id="numberOfUpvotesPlaceholder"></div>
-          <i id="downvotes" className="fa-solid fa-heart-crack"></i>
-        </div>
-      </div>
-    );
+    return <div id="postInfo"></div>;
   }
 };
