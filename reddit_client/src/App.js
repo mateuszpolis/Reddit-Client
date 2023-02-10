c24c239046c01184662dceceeaac792b66ba1691
Mateusz Polis <polismateusz@gmail.com>
2023-02-10 19:29:31 +0100

    Fix viewport problems on some mobile browsers

--- reddit_client/src/App.js
+++ reddit_client/src/App.js
@@ -6,6 +6,16 @@ import { Main } from "./Containers/Main";
 import { Footer } from "./Containers/Footer";
 
 function App() {
+  /**
+   * Fixes the problem of vh not working as intended on some mobile browsers
+   */
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  window.addEventListener("resize", () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+
   return (
     <div className="App">
       <Header />
